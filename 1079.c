667066e59953af4782cb8df07e04ae9ad2c79154
Rafael Telles <raftel.ti@gmail.com>
2018-09-26 14:16:45 -0300

    Add C solution for problem 1079

---
+++ 1079.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int main() {
+    int i, n;
+    double a, b, c;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+    	scanf("%lf %lf %lf", &a, &b, &c);
+        printf("%.1lf\n", (a*2 + b*3 + c*5)/10.0);
+    }
+
+    return 0;
+}
