b2573f2ffdd398de0983156a164eea926c843ca3
Rafael Telles <raftel.ti@gmail.com>
2018-09-26 14:16:45 -0300

    Add C solution for problem 1006

---
+++ 1006.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+        double a, b, c;
+        scanf("%lf", &a);
+        scanf("%lf", &b);
+        scanf("%lf", &c);
+
+        printf("MEDIA = %.1f\n", (2 * a + 3 * b + 5 * c) / 10.0);
+        return 0;
+}
