42098fce1732f7a032ca6701b30305b920092f40
Rafael Telles <raftel.ti@gmail.com>
2018-09-26 14:16:45 -0300

    Add C solution for problem 1052

---
+++ 1052.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+
+    switch(a) {
+        case 1: printf("January\n"); return 0;
+        case 2: printf("February\n"); return 0;
+        case 3: printf("March\n"); return 0;
+        case 4: printf("April\n"); return 0;
+        case 5: printf("May\n"); return 0;
+        case 6: printf("June\n"); return 0;
+        case 7: printf("July\n"); return 0;
+        case 8: printf("August\n"); return 0;
+        case 9: printf("September\n"); return 0;
+        case 10: printf("October\n"); return 0;
+        case 11: printf("November\n"); return 0;
+        case 12: printf("December\n"); return 0;
+    }
+
+	return 0;
+}
